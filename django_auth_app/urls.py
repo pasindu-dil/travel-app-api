49e58e5437dcacaebbe3f386f24b708dd08888c4
pasindu-dil <dilshanpasindu357@gmail.com>
2025-02-24 20:46:59 +0530

    Update token API routes to include versioning in URLs

--- django_auth_app/urls.py
+++ django_auth_app/urls.py
@@ -23,6 +23,6 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('apps.user.urls')),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
